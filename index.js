a0e036b4cef7434aeca5271364123a5d45d41981
bgrullon <bgrullon@redventures.com>
2019-06-24 06:46:49 -0400

    finish javascript portion and last toy problems

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function getInput(){
     inputArr[2] = "N/A";
   }
   inputArr[3] = document.getElementById("esport").value;
-  for(var i = 1; i < 6; i++){
+  for(let i = 1; i < 6; i++){
     if (document.getElementById(i.toString()).checked == true){
       toppings += " " + document.getElementById(i.toString()).value;
     }
@@ -20,5 +20,50 @@ function getInput(){
 
   alert(inputArr[0] + " " + inputArr[1] + "\nGender: " + inputArr[2]
         + "\nFavorite Esport: " + inputArr[3] + "\nFavorite Pizza toppings:" + inputArr[4]);
+
+}
+
+
+function getNameList() {
+  var nameArray = [],
+    nameList = [],
+    lastNameFirst = [],
+    html, newHtml;
+//remove previous ordered list in div from UI
+  var elem = document.getElementById("displayNames");
+  elem.remove();
+//re add ordered list to UI
+  document.querySelector("#very-hard2").insertAdjacentHTML('beforeend', '<ol id="displayNames"> </ol>');
+
+  let names = document.getElementById("nameList").value;
+  //remove whitespace
+  nameArray = names.split(", ");
+
+  console.log(nameArray);
+
+  class FullNames{
+    constructor (fName, lName){
+      this.fName = fName,
+      this.lName = lName;
+    }
+  }
+//loop to swap the positions of first and last name
+  for(let i = 0; i < nameArray.length; i++){
+    let name = [],
+    nameObj;
+    name = nameArray[i].split(" ");
+    nameObj = new FullNames(name[0], name[1]);
+    nameList.push(nameObj);
+    lastNameFirst.push(`${nameList[i].lName}, ${nameList[i].fName}`);
+  }
+  lastNameFirst.sort();
+
+  html = '<li>%name%</li>';
+//inject html to show ordered list
+  for (let i = 0; i < lastNameFirst.length; i++) {
+    newHtml = html.replace('%name%', `${lastNameFirst[i]}`);
+    document.querySelector("#displayNames").insertAdjacentHTML('beforeend', newHtml);
+  }
+
 
 }
